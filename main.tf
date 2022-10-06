d0ebbd4af6a72271f94a74c3392008a7d8bebe5c
Guilherme Schelp <oproprioschelp@gmail.com>
2022-10-06 20:16:49 -0300

    ajuste

---
+++ main.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_version = ">= 1.1.0"
+  required_providers {
+    aws = ">= 4"
+  }
+
+  backend "s3" {
+    bucket = "state-versioning"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  profile                  = "default"
+  shared_credentials_files = ["~/.aws/credentials"]
+  region                   = var.region
+}
+
+
+resource "aws_instance" "web" {
+  ami                     = var.amis[var.region]
+  instance_type           = var.int_type
+  disable_api_termination = var.disable_api_termination
+  user_data               = base64encode(file("./files/userdata.sh"))
+
+  tags = {
+    Name = element(var.int_names, 2)
+  }
+}
+
+resource "aws_instance" "web" {
+  count                   = length(var.int_names)
+  ami                     = var.amis[var.region]
+  instance_type           = var.int_type
+  disable_api_termination = var.disable_api_termination
+  user_data               = file("./files/userdata.sh")
+
+  tags = {
+    Name = var.int_names[count.index]
+  }
+}
+
+resource "aws_instance" "web1" {
+  for_each = {
+    Helloworld  = "t3.nano"
+    Helloworld2 = "t3.micro"
+    Helloworld3 = "t3.small"
+  }
+  ami                     = var.amis[var.region]
+  instance_type           = each.value
+  disable_api_termination = var.disable_api_termination
+  user_data               = file("./files/userdata.sh")
+
+  tags = {
+    Name = each.key
+  }
+}
